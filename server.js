fa53766f0e2262f80326c33bddd57418fd8cda46
Sean <bwellsean@gmail.com>
2025-04-26 19:04:43 -0500

    Update CORS origin to allow requests from the production domain

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "http://sean-blackwell.com",
     methods: ["GET", "POST"],
     allowedHeaders: ["Content-Type"],
   })
